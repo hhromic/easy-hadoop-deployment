96a6f223b30fadb9dab707a12becc9a42f07812b
Hugo Hromic <hhromic@gmail.com>
2015-08-30 01:47:43 +0100

    added customization of *-site.xml configuration files

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,6 +2,7 @@
 # script created by Hugo Hromic
 # https://github.com/hhromic
 
+#===============================================================
 # setup JAVA_HOME
 JAVA_HOME=$(readlink -f `which javac` | sed "s:/bin/javac::")
 JAVA_HOME=${JAVA_HOME%-*}
@@ -11,9 +12,10 @@ if [ ! -z "$NEW_JAVA_HOME" ]; then
 fi
 
 # show this hadoop instance path
-HADOOP_PREFIX=`pwd`
-echo "Hadoop instance: $HADOOP_PREFIX"
+HADOOP_INSTANCE=`pwd`
+echo "Hadoop instance: $HADOOP_INSTANCE"
 
+#===============================================================
 # setup system user for HDFS
 HDFS_USER=hdfs
 read -p "HDFS system user [$HDFS_USER]: " NEW_HDFS_USER
@@ -42,6 +44,15 @@ if [ ! -z "$NEW_HADOOP_GROUP" ]; then
     HADOOP_GROUP=$NEW_HADOOP_GROUP
 fi
 
+#===============================================================
+# setup central server address
+CS_ADDRESS=`hostname`
+read -p "Central server address [$CS_ADDRESS]: " NEW_CS_ADDRESS
+if [ ! -z "$NEW_CS_ADDRESS" ]; then
+    CS_ADDRESS=$NEW_CS_ADDRESS
+fi
+
+#===============================================================
 # create symlinks to the Hadoop binary distribution
 HADOOP_BINARY_DIST=../apache-hadoop
 echo "Hadoop binary distribution: $HADOOP_BINARY_DIST"
@@ -57,8 +68,8 @@ cat <<EOF > etc/vars.sh
 # set this to your local JVM installation
 JAVA_HOME=$JAVA_HOME
 
-# set this to where you unpacked Hadoop
-HADOOP_PREFIX=$HADOOP_PREFIX
+# set this to where you have your local Hadoop instance
+HADOOP_INSTANCE=$HADOOP_INSTANCE
 
 # system users and group
 HDFS_USER=$HDFS_USER
@@ -67,19 +78,77 @@ MAPRED_USER=$MAPRED_USER
 HADOOP_GROUP=$HADOOP_GROUP
 
 # log and pid directories
-HDFS_LOG_DIR=\$HADOOP_PREFIX/logs/\$HDFS_USER
-HDFS_PID_DIR=\$HADOOP_PREFIX/data/\$HDFS_USER
-YARN_LOG_DIR=\$HADOOP_PREFIX/logs/\$YARN_USER
-YARN_PID_DIR=\$HADOOP_PREFIX/data/\$YARN_USER
-MAPRED_LOG_DIR=\$HADOOP_PREFIX/logs/\$MAPRED_USER
-MAPRED_PID_DIR=\$HADOOP_PREFIX/data/\$MAPRED_USER
+HDFS_LOG_DIR=\$HADOOP_INSTANCE/logs/\$HDFS_USER
+HDFS_DATA_DIR=\$HADOOP_INSTANCE/data/\$HDFS_USER
+YARN_LOG_DIR=\$HADOOP_INSTANCE/logs/\$YARN_USER
+YARN_DATA_DIR=\$HADOOP_INSTANCE/data/\$YARN_USER
+MAPRED_LOG_DIR=\$HADOOP_INSTANCE/logs/\$MAPRED_USER
+MAPRED_DATA_DIR=\$HADOOP_INSTANCE/data/\$MAPRED_USER
 EOF
 
-# create log and pid directories
+# create log and data directories
 . etc/vars.sh
-install -o $HDFS_USER -d $HDFS_PID_DIR
-install -o $YARN_USER -d $YARN_PID_DIR
-install -o $MAPRED_USER -d $MAPRED_PID_DIR
 install -o $HDFS_USER -d $HDFS_LOG_DIR
+install -o $HDFS_USER -d $HDFS_DATA_DIR
 install -o $YARN_USER -d $YARN_LOG_DIR
+install -o $YARN_USER -d $YARN_DATA_DIR
 install -o $MAPRED_USER -d $MAPRED_LOG_DIR
+install -o $MAPRED_USER -d $MAPRED_DATA_DIR
+
+# setup the 'core-site.xml' configuration file
+PROPERTIES=$(cat <<EOF | sed -e 's/  /\\ \\ /g' -e ':a;N;$!ba;s/\n/\\n/g'
+  <property>
+    <name>fs.defaultFS</name>
+    <value>hdfs://$CS_ADDRESS</value>
+  </property>
+EOF
+)
+sed -i -e "/<configuration>/a $PROPERTIES" etc/hadoop/core-site.xml
+
+# setup the 'hdfs-site.xml' configuration file
+PROPERTIES=$(cat <<EOF | sed -e 's/  /\\ \\ /g' -e ':a;N;$!ba;s/\n/\\n/g'
+  <property>
+    <name>dfs.namenode.name.dir</name>
+    <value>file://$HDFS_DATA_DIR/name</value>
+  </property>
+  <property>
+    <name>dfs.namenode.checkpoint.dir</name>
+    <value>file://$HDFS_DATA_DIR/namesecondary</value>
+  </property>
+  <property>
+    <name>dfs.datanode.data.dir</name>
+    <value>file://$HDFS_DATA_DIR/data</value>
+  </property>
+  <property>
+    <name>dfs.permissions.superusergroup</name>
+    <value>$HADOOP_GROUP</value>
+  </property>
+EOF
+)
+sed -i -e "/<configuration>/a $PROPERTIES" etc/hadoop/hdfs-site.xml
+
+# setup the 'yarn-site.xml' configuration file
+PROPERTIES=$(cat <<EOF | sed -e 's/  /\\ \\ /g' -e ':a;N;$!ba;s/\n/\\n/g'
+  <property>
+    <name>yarn.resourcemanager.address</name>
+    <value>$CS_ADDRESS:8032</value>
+  </property>
+  <property>
+    <name>yarn.resourcemanager.scheduler.address</name>
+    <value>$CS_ADDRESS:8030</value>
+  </property>
+  <property>
+    <name>yarn.resourcemanager.resource-tracker.address</name>
+    <value>$CS_ADDRESS:8031</value>
+  </property>
+  <property>
+    <name>yarn.resourcemanager.admin.address</name>
+    <value>$CS_ADDRESS:8033</value>
+  </property>
+  <property>
+    <name>yarn.nodemanager.local-dirs</name>
+    <value>$YARN_DATA_DIR/nm-local-dir</value>
+  </property>
+EOF
+)
+sed -i -e "/<configuration>/a $PROPERTIES" etc/hadoop/yarn-site.xml
